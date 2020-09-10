2a1d4cc3f98bc9b9af6e02e4efda586d8a710b97
Manu1ND <manjunath2000@hotmail.com>
2020-09-10 13:56:04 +0530

    db update

--- reservation.sql
+++ reservation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2020 at 03:06 AM
+-- Generation Time: Sep 10, 2020 at 01:55 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -33,53 +33,49 @@ CREATE TABLE `booking` (
   `guestName` varchar(50) NOT NULL,
   `guestEmail` varchar(50) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp(),
-  `numberOfPeople` int(11) NOT NULL
+  `numberOfPeople` int(11) NOT NULL,
+  `roomID` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`tableID`, `slot`, `guestName`, `guestEmail`, `date`, `numberOfPeople`) VALUES
-('1', 1, '', 'manjunath.naik@sakec.ac.in', '2020-09-10', 0),
-('1', 5, '', '', '2020-09-10', 0),
-('1', 22, '', '', '2020-09-10', 0),
-('2', 5, '', '', '2020-09-10', 0),
-('2', 7, '', '', '2020-09-10', 0),
-('2', 23, '', '', '2020-09-10', 0),
-('3', 4, '', '', '2020-09-10', 0),
-('3', 8, '', '', '2020-09-10', 0),
-('3', 23, '', '', '2020-09-10', 0),
-('4', 4, '', '', '2020-09-10', 0),
-('4', 9, '', '', '2020-09-10', 0),
-('4', 11, '', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('4', 19, '', '', '2020-09-10', 0),
-('5', 1, '', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('5', 5, '', '', '2020-09-10', 0),
-('5', 8, '', '', '2020-09-10', 0),
-('5', 11, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('5', 14, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('5', 15, 'manjunayh', 'fde@fd.com', '2020-09-10', 3),
-('5', 16, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('5', 17, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('5', 18, 'manjunayh', 'fde@fd.com', '2020-09-10', 0),
-('5', 19, 'manjunayh', 'fde@fd.com', '2020-09-10', 0),
-('5', 20, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('5', 22, '', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('5', 23, '', '', '2020-09-10', 0),
-('6', 1, '', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('6', 4, '', '', '2020-09-10', 0),
-('6', 8, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('6', 9, '', '', '2020-09-10', 0),
-('6', 11, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('6', 14, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('6', 15, 'manjunayh', 'fde@fd.com', '2020-09-10', 3),
-('6', 16, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('6', 17, '', 'premkatre@gmail.com', '2020-09-10', 0),
-('6', 18, 'manjunayh', 'fde@fd.com', '2020-09-10', 0),
-('6', 19, '', '', '2020-09-10', 0),
-('6', 20, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 0),
-('6', 22, '', 'manjunath2000@hotmail.com', '2020-09-10', 0);
+INSERT INTO `booking` (`tableID`, `slot`, `guestName`, `guestEmail`, `date`, `numberOfPeople`, `roomID`) VALUES
+('1', 1, '', 'manjunath.naik@sakec.ac.in', '2020-09-10', 4, ''),
+('1', 5, '', '', '2020-09-10', 5, ''),
+('1', 22, '', '', '2020-09-10', 3, ''),
+('2', 5, '', '', '2020-09-10', 2, ''),
+('2', 7, '', '', '2020-09-10', 6, ''),
+('2', 23, '', '', '2020-09-10', 5, ''),
+('3', 4, '', '', '2020-09-10', 8, '104'),
+('3', 8, '', '', '2020-09-10', 4, ''),
+('3', 23, '', '', '2020-09-10', 2, ''),
+('4', 4, '', '', '2020-09-10', 4, ''),
+('4', 9, '', '', '2020-09-10', 4, ''),
+('4', 19, '', '', '2020-09-10', 4, ''),
+('5', 5, '', '', '2020-09-10', 4, ''),
+('5', 8, '', '', '2020-09-10', 7, ''),
+('5', 9, 'Manjunath', 'manumanjunath2000@gmail.com', '2020-09-10', 4, '101'),
+('5', 14, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 8, '102'),
+('5', 15, 'manjunayh', 'fde@fd.com', '2020-09-10', 3, ''),
+('5', 16, '', 'premkatre@gmail.com', '2020-09-10', 5, ''),
+('5', 17, '', 'premkatre@gmail.com', '2020-09-10', 4, ''),
+('5', 18, 'manjunayh', 'fde@fd.com', '2020-09-10', 2, ''),
+('5', 19, 'manjunayh', 'fde@fd.com', '2020-09-10', 3, ''),
+('5', 20, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 7, '501'),
+('5', 22, '', 'manjunath2000@hotmail.com', '2020-09-10', 4, ''),
+('5', 23, '', '', '2020-09-10', 5, ''),
+('6', 4, '', '', '2020-09-10', 5, ''),
+('6', 9, '', '', '2020-09-10', 7, ''),
+('6', 14, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 4, ''),
+('6', 15, 'manjunayh', 'fde@fd.com', '2020-09-10', 3, ''),
+('6', 16, '', 'premkatre@gmail.com', '2020-09-10', 3, ''),
+('6', 17, '', 'premkatre@gmail.com', '2020-09-10', 4, ''),
+('6', 18, 'manjunayh', 'fde@fd.com', '2020-09-10', 5, ''),
+('6', 19, '', '', '2020-09-10', 1, ''),
+('6', 20, 'Manjunath', 'manjunath2000@hotmail.com', '2020-09-10', 8, '105'),
+('6', 22, '', 'manjunath2000@hotmail.com', '2020-09-10', 7, '');
 
 -- --------------------------------------------------------
 
